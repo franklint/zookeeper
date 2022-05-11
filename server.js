7f5239d7f6388c6c14fe1a8624e4fb319ed8ff61
franklinT <franklintsung3@gmail.com>
2022-05-10 21:29:22 -0400

    clear message

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 const {animals} = require('./data/animals.json') 
 
+console.log("test"); 
+
 const PORT = process.env.PORT || 3001; 
 const app = express(); 
 
